3476853a971f3cd506fe704396e1a818171b3f48
SafwaanShayk <safwaanshaikh17@gmail.com>
2023-08-13 01:11:38 +0500

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CabManagementSystem.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CabManagementSystem.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users{get;set;}
+    }
+}
